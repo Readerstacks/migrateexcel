c85db0a662f22c62f79c8d4eb0bf980f1cadd0a5
aman <ajain@abnosoftwares.co.ke>
2022-03-08 16:22:35 +0530

    Update in view Template

--- src/Exports/MigrateExportViewSheet.php
+++ src/Exports/MigrateExportViewSheet.php
@@ -4,21 +4,27 @@ namespace Readerstacks\MigrateExcel\Exports;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\BeforeExport;
+use Maatwebsite\Excel\Events\BeforeWriting;
+use Maatwebsite\Excel\Events\BeforeSheet;
+ 
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class MigrateExportViewSheet implements FromView,WithTitle
-// ,WithEvents
+class MigrateExportViewSheet implements FromView,WithTitle ,WithEvents
 {
   
     private $title;
     private $view;
     private $data;
-    
-    public function __construct($title,$view,$data)
+    private $onEvent;
+
+    public function __construct($title,$view,$data,$onEvent)
     {
         $this->title = $title;
         $this->view=$view;
         $this->data=$data;
-        
+        $this->onEvent=$onEvent;
     }
 
     public function view(): View
@@ -34,6 +40,32 @@ class MigrateExportViewSheet implements FromView,WithTitle
     {
         return  $this->title;
     }
+
+
+
+    public function registerEvents(): array
+    {
+        
+        return [
+            // Handle by a closure.
+            BeforeExport::class => function(BeforeExport $event) {
+                $event->writer->getProperties()->setCreator('Patrick');
+            },
+            
+            // Array callable, refering to a static method.
+            BeforeWriting::class => [self::class, 'beforeWriting'],
+            
+            // Using a class with an __invoke method.
+           
+            AfterSheet::class => function(AfterSheet $sheet){
+                $onEvent=$this->onEvent;
+                $onEvent($sheet);    
+                
+            }
+        ];
+    }
+
+     
      
 
      
